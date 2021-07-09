0489c37048bf2967959f297d2bf813cbff2eab4a
Leonardo Yanes <leo95batista@gmail.com>
2021-07-09 10:58:31 -0400

    Fix PHPCodeInsights Errors

--- src/MeteorServiceProvider.php
+++ src/MeteorServiceProvider.php
@@ -13,7 +13,7 @@ class MeteorServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // TODO
     }
 
     /**
@@ -23,6 +23,6 @@ class MeteorServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // TODO
     }
 }
